420260e4269f57bcebdbb8ccc785d07d93b21e64
Roland Tritsch <roland@tritsch.org>
2018-08-30 05:46:10 +0100

    Upgrade to latest eta version.

--- bin/run.sh
+++ bin/run.sh
@@ -1,1 +1,3 @@
-etlas build --enable-test --enable-uberjar-mode && java -Xss1024M -Xmx4096M -jar dist/build/eta-0.8.6.1/eta-aoc-0.1.0.0/t/eta-aoc-test/build/eta-aoc-test/eta-aoc-test.jar && java -Xss1024M -Xmx4096M -jar dist/build/eta-0.8.6.1/eta-aoc-0.1.0.0/x/eta-aoc-exe/build/eta-aoc-exe/eta-aoc-exe.jar
+ETA_VERSION=0.8.6.2
+
+etlas build --enable-test --enable-uberjar-mode && java -Xss1024M -Xmx4096M -jar dist/build/eta-${ETA_VERSION}/eta-aoc-0.1.0.0/t/eta-aoc-test/build/eta-aoc-test/eta-aoc-test.jar && java -Xss1024M -Xmx4096M -jar dist/build/eta-${ETA_VERSION}/eta-aoc-0.1.0.0/x/eta-aoc-exe/build/eta-aoc-exe/eta-aoc-exe.jar
