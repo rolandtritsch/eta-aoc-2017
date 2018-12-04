3148ff0a21f6b8dab23054d7e2a9632c35dc8f5d
Roland Tritsch <roland@tritsch.org>
2018-12-04 19:52:06 +0000

    Remove the uberjar from the run (to make it compile faster).

--- bin/run.sh
+++ bin/run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+ETA_JAVA_ARGS="-Xss1024M -Xmx4096M"
 eta_version=$(etlas exec eta -- --version | cut -d, -f2 | cut -d\  -f3 | sed s/b/./g)
 
 if [ "${TYPE}" = "EXE" ]
 then
   target=eta-aoc-day${DAY}p${PART}
-  etlas build --enable-uberjar-mode ${target}
-  java -Xss1024M -Xmx4096M -jar dist/build/eta-${eta_version}/eta-aoc-0.1.0.0/x/${target}/build/${target}/${target}.jar
+  etlas build ${target} && etlas run ${target}
 else
   target=eta-aoc-test-day${DAY}
-  etlas build --enable-uberjar-mode ${target}
-  java -Xss1024M -Xmx4096M -jar dist/build/eta-${eta_version}/eta-aoc-0.1.0.0/t/${target}/build/${target}/${target}.jar --match "Part${PART}"
+  etlas build ${target} && etlas run ${target} -- --match "Part${PART}"
 fi
